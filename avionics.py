0b5d8019a4f81e3efc68f3d23d584acc24611bb4
Daniele Bella <danielebella2000@gmail.com>
2020-10-31 10:35:16 +0100

    Added Example Code

---
+++ avionics.py
@@ -0,0 +1,11 @@
+# This is an example file
+
+import rocket
+
+if rocket.about_to_crash == true:
+    dont()
+
+# This is some more text.
+# What did you guys have for breakfast?
+# I had cake
+# It wasn't a lie
